9c3966d79372ce7615e8ac003597f3ec78e742bb
hamed <h.sadeghi@metrix.ir>
2022-08-12 13:43:41 +0430

    add validation

--- src/main/java/ir/techco/review/controller/user/request/CommentAddRequest.java
+++ src/main/java/ir/techco/review/controller/user/request/CommentAddRequest.java
@@ -1,8 +1,14 @@
 package ir.techco.review.controller.user.request;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 public class CommentAddRequest {
     private String userId;
     private String text;
+    @Min(value = 0, message = "Point should not be less than 0")
+    @Max(value = 10, message = "Point should not be greater than 10")
+    private Integer point;
 
     public String getUserId() {
         return userId;
@@ -18,5 +24,13 @@ public class CommentAddRequest {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public Integer getPoint() {
+        return point;
+    }
+
+    public void setPoint(Integer point) {
+        this.point = point;
     }
 }
